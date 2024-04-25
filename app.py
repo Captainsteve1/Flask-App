8bb00a9fd419cbd2181e50e43b44ac8fb4df47c1
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2024-04-25 20:22:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, request, abort
 
 app = Flask(__name__)
+API_KEY = 'antoni_bot'
+resources = {'access': 'Enable'}
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def validate_api_key(api_key):
+    return api_key == API_KEY
 
+@app.route('/api/resources', methods=['GET'])
+def get_resources():
+    api_key = request.headers.get('API-Key')
+    if not api_key or not validate_api_key(api_key):
+        abort(403)  # Forbidden
+    else:
+        return jsonify(resources)
+
+@app.route('/api/resources/<int:resource_id>', methods=['GET'])
+def get_resource(resource_id):
+
+    api_key = request.headers.get('API-Key')
+    if not api_key or not validate_api_key(api_key):
+        abort(403)  # Forbidden
+    else:
+        resource = next((res for res in resources if res['id'] == resource_id), None)
+        if resource:
+            return jsonify(resource)
+        else:
+            abort(404)
+
+@app.errorhandler(404)
+def resource_not_found(error):
+    return jsonify({'error': 'Resource not found'}), 404
+
+# Error handler for 403 - Forbidden
+@app.errorhandler(403)
+def forbidden(error):
+    return jsonify({'error': 'Forbidden'}), 403
 
 if __name__ == '__main__':
     app.run(debug=True)
